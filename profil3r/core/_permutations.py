cdeda4972d37de306cb6bd3ccfb555652a2e84ac
aleksejalex <aleksejalex3@gmail.com>
2021-06-22 00:25:30 +0200

    minor changes

--- profil3r/core/_permutations.py
+++ profil3r/core/_permutations.py
@@ -1,5 +1,5 @@
 from itertools import chain, combinations, permutations
-# return all possible permutation for a username
+# return all possible permutations for a username
 # example : ["john", "doe"] -> ("john", "doe", "johndoe", "doejohn", "john.doe", "doe.john")
 
 
@@ -10,9 +10,9 @@ def get_permutations(self):
     for combination in combinations_list:
         for perm in list(permutations(combination)):
 
-            # True if there are two consecutive separators in the permutation, for exemple : ["john", ".", "-", "doe"]
+            # True if there are two consecutive separators in the permutation, for example : ["john", ".", "-", "doe"]
             consecutives_separators = False in [(not perm[i] in self.separators) or (not perm[i + 1] in self.separators) for i in range(len(perm) - 1)]
             
-            # Remove combinations that start or end by a dot or have consecutives separators
+            # Remove combinations that start or end by a dot or have consecutive separators
             if not perm[0] in self.separators and not perm[-1] in self.separators and not consecutives_separators:
                 self.permutations_list.append("".join(perm))
